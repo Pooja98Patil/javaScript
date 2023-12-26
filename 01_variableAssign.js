548302812125c52a417ca7be1c9c133658115c58
Pooja98Patil <ukpooja98@gmail.com>
2023-12-26 09:10:44 +0530

    Added Variable in file
    Added Variable in file

---
+++ 01_variableAssign.js
@@ -0,0 +1,33 @@
+// Point 1 declare a variable for First name
+
+var myFullName; 
+console.log(myFullName);
+myFullName = "Pooja Patil";
+console.log (myFullName);
+myFullName = "Pooja Umesh Patil";
+console.log(myFullName);
+
+// Point 2
+
+var myLove = "I Love Only Js";
+console.log(myLove);
+
+// point 3
+
+var firstName = "Pooja";
+var lastName = "Patil";
+var age = "24";
+var address = "At Post Jalgaon, Maharashatra";
+var pinCode = "425111";
+var gender = "Female";
+var isMarried = "Unmarried";
+
+console.log("First Name:", firstName);
+console.log("Last Name:", lastName);
+console.log("Age:", age);
+console.log("Address:", address);
+console.log("Pin Code:", pinCode);
+console.log("Gender:", gender);
+console.log("Is Married:", Unmarried);
+
+
---
+++ helloWorld.js
@@ -0,0 +1,39 @@
+
+console.log("Hello js, I am Pooja");
+
+var num = "10";
+console.log(num);
+
+var myName = "Pooja";
+console.log(myName);
+
+console.log("First Name:", myName);
+
+myName = "Pooja Patil";
+console.log("First Name:" , myName);
+
+console.log("Hello JS , I am Gajanan");
+
+ 
+
+var myName; // Varibale Declartion
+
+console.log(myName); // undefined
+
+ 
+
+myName = "Gajanan"; // Variable Intialization
+
+console.log("First Name: ",myName);
+
+ 
+
+var age = "32"; // Varibale declaration and Initialization
+
+console.log("Age:", age);
+
+ 
+
+myName = "Gajanan Kharat"; // Varibale update
+
+console.log("Full name: ", myName);
