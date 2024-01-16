6c193b093aff327a0ca50c69401d6fd23f931a7d
Pooja98Patil <ukpooja98@gmail.com>
2024-01-16 22:35:53 +0530

    js assignment

---
+++ javaScript assignment/RemoveDublicateElemnt FromArray.js
@@ -0,0 +1,14 @@
+//////////////////Remove Dublicate Element From Array///////////////////////////////
+console.log(`=========Assignment 1- Remove Dublicate Number From Array===================`);
+const givenNum = [11, 3, 4, 11, 4, 7, 3];
+console.log(`The Given Array => ${givenNum}`);
+const updatedArray = [];
+
+for (let i = 0; i < givenNum.length; i++) {
+  if (updatedArray.indexOf(givenNum[i]) === -1) {
+    updatedArray.push(givenNum[i]);
+  }
+ 
+}
+
+console.log(`After Removing dublicate Value => ${updatedArray}`);
---
+++ javaScript assignment/ToCapitalizeFirst&LastWordOfString.js
@@ -0,0 +1,32 @@
+//////Assignment To convert 1st And last charecter of word in upeerCase/////////////////
+console.log(`=====Assignment To convert 1st And last Charecter of word in upeerCase=====`);
+function inputString(string){
+console.log(`The Given String is => ${string}`);
+let outputString = "";
+
+const words = string.split(" ");
+
+for (let i = 0; i < words.length; i++) {
+    let word = words[i];
+
+    if (word.length > 1) {
+        // Capitalize the first letter
+        word = word.charAt(0).toUpperCase() + word.slice(1);
+//console.log(word);
+        // Capitalize the last letter
+        word = word.slice(0, -1) + word.slice(-1).toUpperCase();
+        //console.log(word);
+    } else {
+        // If the word has only one letter, capitalize it
+        word = word.toUpperCase();
+    }
+
+    outputString += word + " ";
+    
+}
+console.log(`Result => ${outputString}`);
+}
+
+
+
+inputString("how are you mate");
